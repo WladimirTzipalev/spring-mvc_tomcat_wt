1801478c0c5c29f15ad1ddf9088fda0476a498b7
WladimirTzipalev <89722893+WladimirTzipalev@users.noreply.github.com>
2021-12-08 21:36:42 +0300

    Replaced logic to ServiceImpl

--- src/main/java/web/dao/CarDaoImpl.java
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -1,22 +1,12 @@
 package web.dao;
 
 import web.model.Car;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CarDaoImpl implements CarDao {
-    private List<Car> cars = new ArrayList<>();
-
-    {
-        cars.add(new Car ("Chevrolet", "Cruze", 2013));
-        cars.add(new Car ("Toyota", "Camry", 2012));
-        cars.add(new Car ("Dodge", "Journey", 2013));
-        cars.add(new Car ("Skoda", "Octavia", 2010));
-        cars.add(new Car ("Kia", "Sorento", 2019));
-    }
 
     @Override
     public List<Car> getCars(Integer count) {
-        return count != null && count < 5 && count > 0 ? cars.subList(0, count) : cars;
+        return getCars(count);
     }
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -3,13 +3,28 @@ package web.service;
 import web.dao.CarDao;
 import web.dao.CarDaoImpl;
 import web.model.Car;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarServiceImpl implements CarService {
     CarDao carDao = new CarDaoImpl();
+    private List<Car> cars = new ArrayList<>();
+
+    {
+        cars.add(new Car ("Chevrolet", "Cruze", 2013));
+        cars.add(new Car ("Toyota", "Camry", 2012));
+        cars.add(new Car ("Dodge", "Journey", 2013));
+        cars.add(new Car ("Skoda", "Octavia", 2010));
+        cars.add(new Car ("Kia", "Sorento", 2019));
+    }
 
     @Override
     public List<Car> getCars(Integer count) {
-        return carDao.getCars(count);
+        if (count != null && count < 5 && count > 0) {
+            return cars.subList(0, count);
+        } else {
+            return cars;
+        }
     }
 }
